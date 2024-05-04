327774cc03c2b915c92f716df58a0e9b1872311e
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 21:47:42 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿int numero;
+ 
+Console.WriteLine("Digite um número");
+numero = Convert.ToInt32(Console.ReadLine());
+ 
+if (numero < 0)
+{
+Console.WriteLine("Voce digitou um número negativo.");
+}
